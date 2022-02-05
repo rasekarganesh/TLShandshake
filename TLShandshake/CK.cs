1e66ce55e2f632e4a1ef31b00dadb4368c714cb8
rasekarganesh <34262176+rasekarganesh@users.noreply.github.com>
2022-02-05 14:33:05 +0530

    tls
    app

---
+++ TLShandshake/CK.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace TLShandshake
+{
+    public class CK
+    {
+        public bool TryGetCookies(Uri url, out string[] cookies)
+        {
+            List<string> cooks = new List<string>();
+            CookieContainer cooki = new CookieContainer();
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.CookieContainer = cooki;
+            HttpClient client = new HttpClient(handler);
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36");
+            client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            IEnumerable<Cookie> responseCookies = cooki.GetCookies(url).Cast<Cookie>();
+
+            foreach (Cookie cookie in responseCookies)
+            {
+                cooks.Add( cookie.Name + ": " + cookie.Value);
+            }
+            cookies = cooks.ToArray();
+            return true;
+        }
+    }
+}
---
+++ Test.TLShandshake/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TLShandshake;
+
+namespace Test.TLShandshake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CK cK = new CK();
+            string[] cookies;
+            cK.TryGetCookies(new Uri("https://www.google.com/"), out cookies);
+            foreach (var item in cookies)
+            {
+                Console.WriteLine(item);
+            }
+            if (cookies.Length == 0)
+            {
+                Console.WriteLine("No Cookies found.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
